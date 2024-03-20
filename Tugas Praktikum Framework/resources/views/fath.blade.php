0eef0a1d4ab5822a834a2446e02882666e0cd80f
moccachannn <140943824+moccachannn@users.noreply.github.com>
2024-03-20 10:06:08 +0700

    Add files via upload

---
+++ Tugas Praktikum Framework/resources/views/fath.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day6</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../styles/style.css">
+</head>
+<body>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="../images/iyaaaa.jpg" alt="" width="50" height="35">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" arialabel="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Lagu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Album</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="bg-light mt-5" id="main">
+        <!-- Container -->
+            <div class="container py-5 px-4">
+                <div class="row">
+                    <div class="col-md-5 order-md-2">
+                        <img class="img-fluid" src="../images/WhatsApp Image 2024-03-11 at 23.03.04_87b0e33a.jpg" alt="main logo">
+                    </div>
+                    <div class="col-md-7 order-md-1">
+                        <h1 class="mt-4 display-3"><b> HELLO MY DAY!!! </b></h1>
+                        <p class="fs-5 mt-3">
+                        My Day nama penggemar fandom dari Day6 merupakan band pop rock asal korea selatan, dibawah naungan JYP Entertainment.
+                        Band ini terdiri dari lima anggota yaitu: Sungji, Young K, Wonpil, Dowoon, dan jae. Mereka debut pada 7 September 2015.
+                        Lagu mereka sangat diterima oleh pendengar yang tidak menyukai lagu kpop dengan judul lagu Congratulations.
+                        </p>
+                        <div class="row">
+                        <div class="d-flex flex-column flex-md-row">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="footer" class="bg-light py-5">
+    <!-- Container -->
+    <div class="container py-5 px-4">
+        <div class="row">
+            <div class="col-lg-3 mb-5">
+                <a href="#" class="logo text-decoration-none">
+            <div class="d-flex">
+            <img class="img-fluid" src="../images/iyaaaa.jpg" alt="Bootstrap Logo" style="width: 40px;">
+        <div class="fs-5 ms-2 text-black">DAY 6</div>
+    </div>
+</a>
+<div class="mt-2 text-muted">
+    <small>Code licensed <a href="#" class="text-black">MIT</a>, docs <a href="#" class="text-black">CC BY 3.0</a>.</small>
+</div>
+<div class="mt-2 text-muted">
+    <small>Currently v5.3.0-alpha2.</small>
+</div>
+</div>
+<div class="col-6 col-lg-2 offset-lg-1 mb-5">
+    <h5>Lagu</h5>
+    <ul class="list-unstyled">
+    <li class="mb-2"><a href="#">Congratulations/a></li>
+    <li class="mb-2"><a href="#">You Were Beautiful</a></li>
+    <li class="mb-2"><a href="#">For me</a></li>
+    <li class="mb-2"><a href="#">Afraid</a></li>
+    </ul>
+</div>
+<div class="col-6 col-lg-2 mb-5">
+    <h5>Album</h5>
+    <ul class="list-unstyled">
+    <li class="mb-2"><a href="#">The Books of Us: Gravity</a></li>
+    <li class="mb-2"><a href="#">The Book of Us: The Demon/a></li>
+    <li class="mb-2"><a href="#">The Book of Us: Negentropy</a></li>
+    </ul>
+</div>
+</div>
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle
+.min.js"></script>
+</body>
+</html>
